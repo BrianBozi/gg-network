62233c1a373eb104e8466b9bb5fa5d0b043950da
Brian Bozigian <bbozigian@outlook.com>
2021-07-23 07:20:22 +0000

    added functionality where text post will show up on the feed and added some styling

--- client/pages/postForm.jsx
+++ client/pages/postForm.jsx
@@ -20,26 +20,36 @@ export default class NewPost extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    const newDesc = {
-      description: this.state.description
-    };
-    this.props.onsubmit(newDesc);
-    this.setState({ description: '' });
+    fetch('/api/feed/post', {
+      method: 'POST',
+      body: JSON.stringify({ description: this.state.description }),
+      headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+    })
+
+    // reciept of what was sent
+      .then(res => res.json());
+    // .then(post => console.log('post:'.post));
+    // const newDesc = {
+    //   description: this.state.description
+    // };
+    // this.props.onSubmit(newDesc);
+    // this.setState({ description: '' });
   }
 
   render() {
+    // console.log(this.state);
     return (
       <div className="form-container">
         <h3 className="newPostHeader">New Post</h3>
-      <form action="" >
-        <textarea name="" id="" cols="30" rows="10">write your post here</textarea>
+      <form action="" onSubmit={this.handleSubmit}>
+        <textarea required name="" id="" cols="30" rows="10" onChange={this.handleChange} placeholder="write your post here"></textarea>
         <div>
           <div className="row">
           <div className="col-half">
-          <button className="cancel"><a href="#">cancel</a></button>
+          <a className="cancel" href="#">CANCEL</a>
           </div>
           <div className="col-half">
-          <button type="submit">SUBMIT</button>
+          <button type="submit" className="submit">SUBMIT</button>
           </div>
           </div>
         </div>
--- server/index.js
+++ server/index.js
@@ -39,6 +39,30 @@ app.get('/api/feed', (req, res) => {
     });
 });
 
+app.post('/api/feed/post', (req, res) => {
+  const { description } = req.body;
+  // console.log('description:', description);
+  if (!description) {
+    res.status(400).json({
+      error: 'description needs to be filled out'
+    });
+    res.json({ description: 'this is working' });
+  }
+  // I AM USING VALUE 2 AS A PLACE HOLDER FOR TESTING TO SEE IF THE POSTING WILL WORK
+  const sql = `
+  insert into "posts" ("description", "userId")
+  values ($1, 2)
+  returning *
+  `;
+  // YES BRIAN THE @ ABOVE HERE!!!!
+  const params = [description];
+  db.query(sql, params)
+    .then(result => {
+      const [post] = result.rows;
+      res.status(201).json(post);
+    });
+});
+
 app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
